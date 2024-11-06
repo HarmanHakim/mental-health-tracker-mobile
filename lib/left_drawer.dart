3abb172900133ac3b57c109e92e4638da5e6c266
HarmanHakim <harmanhakim99@gmail.com>
2024-11-06 14:20:00 +0700

    tutorial 7

--- lib/left_drawer.dart
+++ lib/left_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/menu.dart';
+import 'package:mental_health_tracker_mobile/screens/menu.dart';
 // TODO: Impor halaman MoodEntryFormPage jika sudah dibuat
 
 class LeftDrawer extends StatelessWidget {
